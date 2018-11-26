6625aa8f1919787dc7ec73371890f42bd3de8ae0
LaurentiuMolnar <lau.molnar2013@gmail.com>
2018-11-26 16:03:54 +0200

    Fix AT_CSQ command parser

---
+++ at.h
@@ -0,0 +1,38 @@
+#ifndef AT_H
+#define AT_H
+
+#define INIT_STATE 0
+#define STATE_1 1
+#define STATE_2 2
+#define STATE_3 3
+#define STATE_4 4
+#define STATE_5 5
+#define STATE_6 6
+#define STATE_7 7
+#define STATE_8 8
+#define STATE_9 9
+#define STATE_10 10
+#define STATE_11 11
+#define SUCCESS_STATE 12
+#define STATE_13 13
+#define STATE_14 14
+#define STATE_15 15
+#define STATE_16 16
+#define ERROR_STATE 17
+
+#define STR_CNT 100
+#define STR_SIZE 200
+
+typedef struct {
+  char data[STR_CNT][STR_SIZE + 1];
+  uint16_t line_count;
+  bool ok;
+} AT_DATA;
+
+extern AT_DATA data;
+
+void print_data();
+
+uint8_t parse(char current_char);
+
+#endif
---
+++ main.c
@@ -0,0 +1,277 @@
+#include <stdint.h>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include "LPC177x_8x.h"
+#include "system_LPC177x_8x.h"
+#include <retarget.h>
+
+#include <DRV\drv_sdram.h>
+#include <DRV\drv_lcd.h>
+#include <DRV\drv_uart.h>
+#include <DRV\drv_touchscreen.h>
+#include <DRV\drv_led.h>
+#include <utils\timer_software.h>
+#include <utils\timer_software_init.h>
+
+#include "at.h"
+
+char at_command_at[] = "AT\r\n";
+char at_command_csq[] = "AT+CSQ\r\n";
+
+timer_software_handler_t handler;
+
+void timer_callback_1(timer_software_handler_t h)
+{
+}
+
+/*
+void testLCD()
+{
+	uint32_t i,j;
+	LCD_PIXEL foreground = {0, 255, 0, 0};
+	LCD_PIXEL background = {0, 0, 0, 0};
+	
+	
+	for (i = 0; i < LCD_HEIGHT; i++)
+	{
+		for (j = 0; j < LCD_WIDTH / 3; j++)
+		{
+			DRV_LCD_PutPixel(i, j, 255, 0, 0);
+		}
+		for (j = LCD_WIDTH / 3; j < 2 * (LCD_WIDTH / 3); j++)
+		{
+			DRV_LCD_PutPixel(i, j, 230, 220, 0);
+		}
+		for (j = 2 * (LCD_WIDTH / 3); j < LCD_WIDTH; j++)
+		{
+			DRV_LCD_PutPixel(i, j, 0, 0, 255);
+		}
+	}
+
+	DRV_LCD_Puts("Hello", 20, 30, foreground, background, TRUE);
+	DRV_LCD_Puts("Hello", 20, 60, foreground, background, FALSE);	
+}
+*/
+void TouchScreenCallBack(TouchResult* touchData)
+{
+	printf("touched X=%3d Y=%3d\n", touchData->X, touchData->Y);		
+	
+}
+
+void BoardInit()
+{
+	//timer_software_handler_t handler;
+	
+	TIMER_SOFTWARE_init_system();
+	
+	
+	DRV_SDRAM_Init();
+	
+	initRetargetDebugSystem();
+	DRV_LCD_Init();
+	DRV_LCD_ClrScr();
+	DRV_LCD_PowerOn();	
+	
+	DRV_TOUCHSCREEN_Init();
+	DRV_TOUCHSCREEN_SetTouchCallback(TouchScreenCallBack);
+	DRV_LED_Init();
+	printf ("Hello\n");	
+	
+	//handler = TIMER_SOFTWARE_request_timer();
+//	TIMER_SOFTWARE_configure_timer(handler, MODE_1, 100, 1);
+	//TIMER_SOFTWARE_set_callback(handler, timer_callback_1);
+//	TIMER_SOFTWARE_start_timer(handler);
+}
+
+void send_command(char *cmd) {
+	uint8_t ch;
+	
+	DRV_UART_FlushRX(UART_3);
+	DRV_UART_FlushTX(UART_3);
+	//printf("execute_command\n");
+	DRV_UART_Write(UART_3, (uint8_t*) cmd, strlen(cmd)); 
+	
+//	while(1) {
+//		while(DRV_UART_ReadByte(UART_3, &ch) == OK) {
+//			printf("%c", ch);
+//		}
+//	}
+}
+
+void get_command_response() {
+	uint8_t ch;
+	uint8_t state = 0;
+	handler = TIMER_SOFTWARE_request_timer();
+	TIMER_SOFTWARE_configure_timer(handler, MODE_1, 10000, 1);
+	
+	TIMER_SOFTWARE_reset_timer(handler);
+	TIMER_SOFTWARE_start_timer(handler);
+	
+	while(!TIMER_SOFTWARE_interrupt_pending(handler)){
+		
+		while(DRV_UART_BytesAvailable(UART_3) > 0 && (state != ERROR_STATE)) {
+			DRV_UART_ReadByte(UART_3, &ch);
+			//printf("%c", ch);
+			state = parse(ch);
+		}
+	}
+}
+
+void execute_command(char *cmd) {
+	send_command(cmd);
+	get_command_response();
+}
+
+bool verify_response(AT_DATA *data) {	
+	return data->ok;
+}
+
+uint32_t tol(char *s) {
+	uint32_t nr = 0, i = 0;
+	while(s[i] != 0) {
+		if(s[i] >= '0' && s[i] <= '9') {
+			nr = nr*10 + (s[i] - '0');
+		}
+		if(s[i] == ',') return nr;
+		i++;
+	}
+	return nr;
+}
+
+uint32_t get_asu_from_response(AT_DATA *data){
+	char *text;
+	//printf("%ld\n", tol(data->data[0]));
+	//return tol(data->data[0]);
+	return strtol(&data->data[0][6], NULL, 10);
+}
+int32_t asu_to_dbmw(uint32_t asu) {
+	return 2*asu - 113;
+}
+int main(void)
+{
+	//uint8_t ch;
+	uint32_t rssi_value_asu;
+	uint32_t rssi_value_dbmw;
+	
+	BoardInit();
+	//testLCD();
+	
+	DRV_UART_Configure(UART_3, UART_CHARACTER_LENGTH_8, 115200, UART_PARITY_NO_PARITY, 1, TRUE, 3);
+	//DRV_UART_Configure(UART_2, UART_CHARACTER_LENGTH_8, 115200, UART_PARITY_NO_PARITY, 1, TRUE, 3);
+	
+	handler = TIMER_SOFTWARE_request_timer();
+	TIMER_SOFTWARE_configure_timer(handler, MODE_1, 1000, 1);
+	// send AT command to tell modem to autobaud
+	send_command(at_command_at);
+	TIMER_SOFTWARE_Wait(1000);
+	//print_data();
+	
+	send_command(at_command_at);
+	TIMER_SOFTWARE_Wait(1000);
+	//print_data();
+	
+	send_command(at_command_at);
+	TIMER_SOFTWARE_Wait(1000);
+	//print_data();
+	
+	TIMER_SOFTWARE_reset_timer(handler);
+	TIMER_SOFTWARE_start_timer(handler);
+	
+	while(1) {
+		
+		if(TIMER_SOFTWARE_interrupt_pending(handler)) {
+			execute_command(at_command_csq);
+			print_data();
+			if(verify_response(&data)) {
+				rssi_value_asu = get_asu_from_response(&data);
+				rssi_value_dbmw = asu_to_dbmw(rssi_value_asu);
+				printf("GSM modem signal %"PRIu32" ASU -> %"PRIi32" dBmW\n", rssi_value_asu, rssi_value_dbmw);
+			}
+			//execute_command(at_command_at);
+		//	print_data();
+			TIMER_SOFTWARE_clear_interrupt(handler);
+		}
+		
+	}
+	
+	
+	
+/*	while(1)
+	{
+		DRV_LED_Toggle(LED_4);
+		
+	}
+	
+	while(1)
+	{
+		DRV_UART_SendByte(UART_3, 'A');
+	//	TIMER_SOFTWARE_Wait(1000);
+	}
+	*/
+	/*
+	while(1)
+	{
+		if (DRV_UART_ReadByte(UART_3, &ch) == OK)
+		{
+			DRV_UART_SendByte(UART_3, ch);
+		}		
+	}
+
+	while(1)
+	{
+		if (DRV_UART_ReadByte(UART_0, &ch) == OK)
+		{
+			DRV_UART_SendByte(UART_3, ch);
+		}
+		if (DRV_UART_ReadByte(UART_3, &ch) == OK)
+		{
+			DRV_UART_SendByte(UART_0, ch);
+		}
+		if (DRV_UART_ReadByte(UART_2, &ch) == OK)
+		{
+			DRV_UART_SendByte(UART_0, ch);
+		}
+	}
+	
+	while(1)
+	{
+		DRV_UART_Process();
+		DRV_TOUCHSCREEN_Process();
+	}
+*/
+	return 0; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
