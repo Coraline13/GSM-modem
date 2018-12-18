f7d60588cf6bcd08e4bfe39946dbb90de02b23eb
LaurentiuMolnar <lau.molnar2013@gmail.com>
2018-12-18 15:31:43 +0200

    Add button touch detection

--- AT/AT/code/main.c
+++ AT/AT/code/main.c
@@ -29,12 +29,15 @@ char at_command_gmr[] = "AT+GMR\r\n";
 char at_command_extended_creg_1[] = "AT+CREG=1\r\n";
 char at_command_extended_creg_2[] = "AT+CREG=2\r\n";
 
+char at_command_cmgl[] = "AT+CMGL=\"ALL\"\r\n";
+
 LCD_PIXEL white = {255, 255, 255};
 LCD_PIXEL primary = {95, 75, 139};
 LCD_PIXEL black = {0, 0, 0};
 
 timer_software_handler_t handler_main;
 timer_software_handler_t handler_get_response;
+timer_software_handler_t handler_buttons;
 
 void timer_callback_1(timer_software_handler_t h)
 {
@@ -42,13 +45,32 @@ void timer_callback_1(timer_software_handler_t h)
 
 void TouchScreenCallBack(TouchResult* touchData)
 {
-	printf("touched X=%3d Y=%3d\n", touchData->X, touchData->Y);	
+	//printf("touched X=%3d Y=%3d\n", touchData->X, touchData->Y);	
+	handler_buttons = TIMER_SOFTWARE_request_timer();
+	TIMER_SOFTWARE_configure_timer(handler_buttons, MODE_1, 200, 1);
+	
+	TIMER_SOFTWARE_reset_timer(handler_buttons);
+	TIMER_SOFTWARE_Wait(200);
+	
+	if(touchData->Y >= 200) {
+		if(touchData->X >= 0 && touchData->X <= LCD_WIDTH/4) {
+			printf("<Prev button was pressed\n");
+		} else if(touchData->X >= LCD_WIDTH/4 && touchData->X <= LCD_WIDTH/2) {
+			printf("Delete button was pressed\n");
+		} else if(touchData->X >= LCD_WIDTH/2 && touchData->X <= 3*LCD_WIDTH/4) {
+			printf("Send button was pressed\n");
+		} else {
+			printf("Next> button was pressed\n");
+		}
+	}
 }
 
 void drawButtons(uint8_t, uint8_t, uint16_t, uint16_t, LCD_PIXEL);
 
 void BoardInit()
 {		
+	int i, j;
+	
 	TIMER_SOFTWARE_init_system();
 	
 	DRV_SDRAM_Init();
@@ -67,8 +89,16 @@ void BoardInit()
 	DRV_LCD_TestFillColor(255, 255, 255);
 	drawButtons(0, 200, LCD_WIDTH, LCD_HEIGHT, primary);
 	
+	for(i = 0; i <= 40; i++) {
+		for(j = 0; j <= LCD_WIDTH; j++) {
+			DRV_LCD_PutPixel(i, j, primary.red, primary.green, primary.blue);
+		}
+	}
+	DRV_LCD_Puts("GSM modem signal: <acquiring data>", 20, 5, white, primary, 0);
+	DRV_LCD_Puts("Network state: <acquiring data>", 20, 18, white, primary, 0);
+	DRV_LCD_Puts("Network operator name: <acquiring data>", 20, 30, white, primary, 0);
 }
-
+              
 void send_command(char *cmd) {
 	DRV_UART_FlushRX(UART_3);
 	DRV_UART_FlushTX(UART_3);
@@ -113,7 +143,7 @@ int32_t asu_to_dbmw(uint32_t asu) {
 // for AT_COPS
 char* get_operator_name(AT_DATA *data) {
 	char *op_name;
-	op_name = strstr(&data->data[0][8], ",");
+	op_name = strstr(&data->data[0][8], "\"");
 	return op_name;
 }
 
@@ -165,13 +195,9 @@ int main(void)
 {
 	uint32_t rssi_value_asu;
 	int32_t rssi_value_dbmw;
-	int32_t previous_dbmw = 0;
-	char *previous_op_name;
-	char *previous_network_state;
+	char s[100];
+	int i,j;
 
-	strcpy(previous_op_name, "");
-	strcpy(previous_network_state, "Unknown state");
-	
 	BoardInit();
 	
 	DRV_UART_Configure(UART_3, UART_CHARACTER_LENGTH_8, 115200, UART_PARITY_NO_PARITY, 1, TRUE, 3);
@@ -191,59 +217,70 @@ int main(void)
 	TIMER_SOFTWARE_reset_timer(handler_main);
 	TIMER_SOFTWARE_start_timer(handler_main);
 	
-	while (1) {		
+	for(i = 0; i <= 40; i++) {
+		for(j = 0; j <= LCD_WIDTH; j++) {
+			DRV_LCD_PutPixel(i, j, primary.red, primary.green, primary.blue);
+		}
+	}
+	
+	execute_command(at_command_cmgl, AT_CSQ);
+			print_data();
+			if(verify_response(&data)) {
+				print_data();
+			}
+	
+	while (1) {	
+				
 		if (TIMER_SOFTWARE_interrupt_pending(handler_main)) {
+			//printf("Sunt aici\n");
 			execute_command(at_command_csq, AT_CSQ);
+			//print_data();
 			if (verify_response(&data)) {
 				rssi_value_asu = get_asu_from_response(&data);
 				rssi_value_dbmw = asu_to_dbmw(rssi_value_asu);
-			//	printf("GSM modem signal %"PRIu32" ASU -> %"PRIi32" dBmW\n", rssi_value_asu, rssi_value_dbmw);
-				if (previous_dmbw != rssi_value_dbmw) {
-					printf("GSM signal strength: %"PRIi32" dBmW\n", rssi_value_dbmw);
-				}
-				previous_dbmw = rssi_value_dbmw;
+				//printf("GSM modem signal %"PRIu32" ASU -> %"PRIi32" dBmW\n", rssi_value_asu, rssi_value_dbmw);
+				sprintf(s, "GSM modem signal %"PRIi32" dBmW", rssi_value_dbmw);
+				DRV_LCD_Puts(s, 20, 5, white, primary, 0);
 			}
-			
+			/*
 			execute_command(at_command_gsn, AT_GSN);
 			if (verify_response(&data)) {
-		//		printf("Modem IMEI: %s\n", get_imei(&data));
+				printf("Modem IMEI: %s\n", get_imei(&data));
 			}
-			
+			*/
 			execute_command(at_command_extended_creg_1, AT_CREG);
 			execute_command(at_command_creg, AT_CREG);
 			if(verify_response(&data)){
-		//		printf("Network state: %s\n", get_network_state(&data));
-				if (strcmp(previous_network_state, get_network_state(&data))) {
-					printf("Network state: %s\n", get_network_state(&data));
-				}
-				strcpy(previous_network_state, get_network_state(&data));
+				//printf("Network state: %s\n", get_network_state(&data));
+				sprintf(s, "Network state: %s", get_network_state(&data));
+				DRV_LCD_Puts(s, 20, 18, white, primary, 0);
 			}
 			
 			execute_command(at_command_cops, AT_COPS);
 			if(verify_response(&data)) {
-		//		printf("Network operator name: %s\n", get_operator_name(&data));
-				if (strcmp(previous_op_name, get_operator_name(&data)) != 0) {
-					printf("Operator: %s\n", get_operator_name(&data));
-				}
-				strcpy(previous_op_name, get_operator_name(&data));
+			//	print_data();
+				//printf("Network operator name: %s\n", get_operator_name(&data));
+				sprintf(s, "Network operator name: %s", get_operator_name(&data));
+				DRV_LCD_Puts(s, 20, 30, white, primary, 0);
 			}
-			
+			/*
 			execute_command(at_command_gmi, AT_GMI);
 			if(verify_response(&data)) {
-		//		printf("Modem manufacturer: %s\n", get_manufacturer_identity(&data));
+				printf("Modem manufacturer: %s\n", get_manufacturer_identity(&data));
 			}
 			
 			execute_command(at_command_gmr, AT_GMR);
 			if(verify_response(&data)) {
-		//		printf("Modem software version: %s\n", get_software_version(&data));
+				printf("Modem software version: %s\n", get_software_version(&data));
 			}
-
+			*/
 			/*
 			execute_command(at_command_extended_creg_2, AT_CREG);
 			print_data();
 			execute_command(at_command_creg, AT_CREG);
 			print_data();
 			*/
+			
 
 			TIMER_SOFTWARE_clear_interrupt(handler_main);
 
